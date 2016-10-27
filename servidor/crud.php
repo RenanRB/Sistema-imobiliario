428f957430d3f9c3f47ae9dc5e587d24123f1289
Renan Rafael Bertoldo <renanrafaelbertoldo@gmail.com>
2016-10-27 19:58:19 -0200

    Alteração para PHP

--- servidor/crud.php
+++ servidor/crud.php
@@ -1,96 +1,91 @@
 <?php
+
 require_once("autenticador.php");
 
+$pdo = new PDO('mysql:host=localhost;dbname=sistema_imobiliario', 'root', '');
+
+
 if (isset($_GET['acao']) && $_GET['acao'] == 'editar') {
-	$idUsuario = $_GET['id_usuario'];
-	
-	$getfile = file_get_contents('../dados/usuarios.json');
-	$jsonfile = json_decode($getfile, true);
-	
-	$jsonfileUser = $jsonfile["usuarios"];
-	$usuario = $jsonfileUser[$idUsuario];
-	echo json_encode($usuario);
-	
+
+	$stmt = $pdo->prepare("select id, email, nome, dt_nascimento from usuarios where id = :id");
+	$stmt->bindValue(':id', $_GET['id_usuario']);
+	$stmt->execute();
+	$results = $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
+	$results['dt_nascimento'] = implode("/", array_reverse(explode("-", $results['dt_nascimento'])));
+	echo json_encode($results);
+
 } elseif(isset($_GET['acao']) && $_GET['acao'] == 'listar') {
-	$getfile = file_get_contents('../dados/usuarios.json');
-	$jsonfile = json_decode($getfile, true);
-	$usuarios = $jsonfile['usuarios'];
-	$retorno = Array();
-	
-	foreach ( $usuarios as $chave => $e ) { 
-		array_push($retorno, Array("chave"=>$chave, "nome"=>$e['nome']));
-	}
-	
-	echo json_encode($retorno);
-	
+	$stmt = $pdo->prepare("select id, email, nome from usuarios");
+	$stmt->execute();
+	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	echo json_encode($results);
+
 } elseif (isset($_POST['acao']) && $_POST['acao'] == 'alterar') {
-	$getfile = file_get_contents('../dados/usuarios.json');
-	$jsonfile = json_decode($getfile, true);
-	
-	$usuarios = $jsonfile['usuarios'];
-	$usuarios[$_POST['id']]['nome'] = $_POST['nome'];
-	$usuarios[$_POST['id']]['email'] = $_POST['email'];
-	$usuarios[$_POST['id']]['dt_nascimento'] = $_POST['dtNascimento'];
-	$usuarios[$_POST['id']]['senha'] = $_POST['senha'];
-	$response['usuarios'] = $usuarios;
-	
-	$fp = fopen('../dados/usuarios.json', 'w');
-	fwrite($fp, json_encode($response));
-	fclose($fp);
-} elseif (isset($_POST['acao']) && $_POST['acao'] == 'excluir') {
-	$getfile = file_get_contents('../dados/usuarios.json');
-	$jsonfile = json_decode($getfile, true);
-	
-	$usuarios = $jsonfile['usuarios'];
-	unset($usuarios[$_POST['id_usuario']]);
-	$response['usuarios'] = $usuarios;
-	
-	$fp = fopen('../dados/usuarios.json', 'w');
-	fwrite($fp, json_encode($response));
-	fclose($fp);
-} elseif (isset($_POST['acao']) && $_POST['acao'] == 'cadastrar') {
-	$getfile = file_get_contents('../dados/usuarios.json');
-	$jsonfile = json_decode($getfile, true);
-	
-	$usuarios = $jsonfile['usuarios'];
-	array_push($usuarios, Array('nome' => $_POST['nome'],
-								'email' => $_POST['email'],
-								'dt_nascimento' => $_POST['dtNascimento'],
-								'senha' => $_POST['senha']));
-	$response['usuarios'] = $usuarios;
-	
-	$fp = fopen('../dados/usuarios.json', 'w');
-	fwrite($fp, json_encode($response));
-	fclose($fp);
-} elseif (isset($_POST['acao']) && $_POST['acao'] == 'login') {
-	$getfile = file_get_contents('../dados/usuarios.json');
-	$jsonfile = json_decode($getfile, true);
-	$usuarios = $jsonfile['usuarios'];
-	
-	foreach ( $usuarios as $chave => $e ) {
-		if ($e['email'] == $_POST['email']) {
-			if ($e['senha'] == $_POST['senha']) {
-				session_start();
-				$_SESSION['id'] = $chave;
-				$_SESSION['nome'] = $e['nome'];
-				$_SESSION['email'] = $e['email'];
-				echo "ok";
-			} else {
-				echo "senhaInvalida";
-			}
-		}
+	if ($_POST['senha'] != ''){
+		$stmt = $pdo->prepare("UPDATE usuarios set nome = ?, email = ?, dt_nascimento = ?, senha = ? WHERE id = ?");
+		$stmt->bindParam(1, $_POST['nome']);
+		$stmt->bindParam(2, $_POST['email']);
+		$stmt->bindParam(3, implode("-", array_reverse(explode("/", $_POST['dtNascimento']))));
+		$stmt->bindParam(4, md5(sha1($_POST['senha'])));
+		$stmt->bindParam(5, $_POST['id']);
+	} else {
+		$stmt = $pdo->prepare("UPDATE usuarios set nome = ?, email = ?, dt_nascimento = ? WHERE id = ?");
+		$stmt->bindParam(1, $_POST['nome']);
+		$stmt->bindParam(2, $_POST['email']);
+		$stmt->bindParam(3, implode("-", array_reverse(explode("/", $_POST['dtNascimento']))));
+		$stmt->bindParam(4, $_POST['id']);
 	}
+
+	$stmt->execute();
+
+} elseif (isset($_POST['acao']) && $_POST['acao'] == 'excluir') {
+
+	$stmt = $pdo->prepare("delete from usuarios where id = :id");
+	$stmt->bindValue(':id', $_POST['id_usuario']);
+	$stmt->execute();
+
+} elseif (isset($_POST['acao']) && $_POST['acao'] == 'cadastrar') {
+
+	$stmt = $pdo->prepare("INSERT INTO usuarios (nome, email, dt_nascimento, senha, ativo, nivel) VALUES (?, ?, ?, ?, 1, 0)");
+	$stmt->bindParam(1, $_POST['nome']);
+	$stmt->bindParam(2, $_POST['email']);
+	$stmt->bindParam(3, implode("-", array_reverse(explode("/", $_POST['dtNascimento']))));
+	$stmt->bindParam(4, md5(sha1($_POST['senha'])));
+	$stmt->execute();
+
+} elseif (isset($_POST['acao']) && $_POST['acao'] == 'login') {
+
+	$stmt = $pdo->prepare("select id, email, nome from usuarios where email = :login and senha = :senha");
+	$stmt->bindValue(':login', $_POST['email']);
+	$stmt->bindValue(':senha', md5(sha1($_POST['senha'])));
+	if($stmt->execute()){
+		if($stmt->rowCount() > 0){
+			$result = $stmt->fetch(PDO::FETCH_OBJ);
+
+			session_start();
+
+			$_SESSION['id'] = $result->id;
+			$_SESSION['nome'] = $result->nome;
+			$_SESSION['email'] = $result->email;
+			echo "ok";
+		} else {
+			echo "senhaInvalida";
+		}
+	} else {
+		echo "probConexao";
+	}
+
 } elseif (isset($_POST['acao']) && $_POST['acao'] == 'cadastrarImovel') {
 	$getfile = file_get_contents('../dados/usuarios.json');
 	$jsonfile = json_decode($getfile, true);
-	
+
 	$usuarios = $jsonfile['usuarios'];
 	$usuario = $usuarios[$_SESSION['id']];
-	
+
 	if (!isset($usuario['imoveis'])) {
 		$usuario['imoveis'] = Array();
 	}
-	
+
 	array_push($usuario['imoveis'], Array('nome' => $_POST['nome'],
 								'suites' => $_POST['suites'],
 								'quartos' => $_POST['quartos'],
@@ -105,10 +100,10 @@ if (isset($_GET['acao']) && $_GET['acao'] == 'editar') {
 								'numero' => $_POST['numero'],
 								'caracteristicas' => $_POST['caracteristicas'],
 								'informacoes_adicionais' => $_POST['informacoes_adicionais']));
-								
+
 	$usuarios[$_SESSION['id']] = $usuario;
 	$response['usuarios'] = $usuarios;
-	
+
 	$fp = fopen('../dados/usuarios.json', 'w');
 	fwrite($fp, json_encode($response));
 	fclose($fp);
@@ -116,9 +111,9 @@ if (isset($_GET['acao']) && $_GET['acao'] == 'editar') {
 	$getfile = file_get_contents('../dados/usuarios.json');
 	$jsonfile = json_decode($getfile, true);
 	$usuarios = $jsonfile['usuarios'];
-	
+
 	$htmlImoveis = '';
-	
+
 	foreach ( $usuarios as $chave => $e ) {
 		if(isset($e['imoveis'])) {
 			foreach ($e['imoveis'] as $chaveImovel => $ee) {
@@ -133,7 +128,7 @@ if (isset($_GET['acao']) && $_GET['acao'] == 'editar') {
 						</div>
 					</div>
 					<div class="informacoes-busca col-md-3 col-sm-3 col-xs-12">
-					
+
 						<button type="button" class="btn-busca" onclick="editarImovel(' . $chaveImovel . ', ' . $chave . ');">Editar</button>
 						<button type="button" class="btn-busca" onclick="excluirImovel(' . $chaveImovel . ', ' . $chave . ');" data-toggle="modal" data-target="#excluir_imovel">Excluir</button>
 					</div>
@@ -141,32 +136,32 @@ if (isset($_GET['acao']) && $_GET['acao'] == 'editar') {
 			}
 		}
 	}
-	
+
 	echo $htmlImoveis;
 } elseif (isset($_POST['acao']) && $_POST['acao'] == 'excluirImovel') {
 	$getfile = file_get_contents('../dados/usuarios.json');
 	$jsonfile = json_decode($getfile, true);
-	
+
 	$usuarios = $jsonfile['usuarios'];
 	unset($usuarios[$_POST['id_usuario']]['imoveis'][$_POST['id_imovel']]);
 	$response['usuarios'] = $usuarios;
-	
+
 	$fp = fopen('../dados/usuarios.json', 'w');
 	fwrite($fp, json_encode($response));
 	fclose($fp);
-	
+
 } elseif (isset($_POST['acao']) && $_POST['acao'] == 'buscaEditarImovel') {
 	$getfile = file_get_contents('../dados/usuarios.json');
 	$jsonfile = json_decode($getfile, true);
-	
+
 	$usuarios = $jsonfile['usuarios'];
 	echo json_encode($usuarios[$_POST['id_usuario']]['imoveis'][$_POST['id_imovel']]);
 } elseif (isset($_POST['acao']) && $_POST['acao'] == 'editarImovel') {
 	$getfile = file_get_contents('../dados/usuarios.json');
 	$jsonfile = json_decode($getfile, true);
-	
+
 	$usuarios = $jsonfile['usuarios'];
-	
+
 	$usuarios[$_POST['id_usuario']]['imoveis'][$_POST['id_imovel']] = Array(
 								'nome' => $_POST['nome'],
 								'suites' => $_POST['suites'],
@@ -182,12 +177,12 @@ if (isset($_GET['acao']) && $_GET['acao'] == 'editar') {
 								'numero' => $_POST['numero'],
 								'caracteristicas' => $_POST['caracteristicas'],
 								'informacoes_adicionais' => $_POST['informacoes_adicionais']);
-								
+
 	$response['usuarios'] = $usuarios;
-	
+
 	$fp = fopen('../dados/usuarios.json', 'w');
 	fwrite($fp, json_encode($response));
 	fclose($fp);
-}
 
+}
 ?>
