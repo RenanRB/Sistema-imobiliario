1b86c212959aa6c3750b1130ba95443a6280f9da
Renan Rafael Bertoldo <renanrafaelbertoldo@gmail.com>
2016-11-03 19:15:19 -0200

    Finalização crud

--- servidor/crud.php
+++ servidor/crud.php
@@ -1,9 +1,9 @@
 <?php
-
+header("Content-Type: text/html; charset=UTF-8",true);
 require_once("autenticador.php");
 
 $pdo = new PDO('mysql:host=localhost;dbname=sistema_imobiliario', 'root', '');
-
+$pdo->exec("set names utf8");
 
 if (isset($_GET['acao']) && $_GET['acao'] == 'editar') {
 
@@ -15,13 +15,14 @@ if (isset($_GET['acao']) && $_GET['acao'] == 'editar') {
 	echo json_encode($results);
 
 } elseif(isset($_GET['acao']) && $_GET['acao'] == 'listar') {
+
 	$stmt = $pdo->prepare("select id, email, nome from usuarios");
 	$stmt->execute();
 	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	echo json_encode($results);
 
 } elseif (isset($_POST['acao']) && $_POST['acao'] == 'alterar') {
-	
+
 	if ($_POST['senha'] != ''){
 		$stmt = $pdo->prepare("UPDATE usuarios set nome = ?, email = ?, dt_nascimento = ?, senha = ? WHERE id = ?");
 		$stmt->bindParam(1, $_POST['nome']);
@@ -77,113 +78,132 @@ if (isset($_GET['acao']) && $_GET['acao'] == 'editar') {
 	}
 
 } elseif (isset($_POST['acao']) && $_POST['acao'] == 'cadastrarImovel') {
-	$getfile = file_get_contents('../dados/usuarios.json');
-	$jsonfile = json_decode($getfile, true);
+	$stmt = $pdo->prepare("select id from enderecos where cep = :cep");
+	$stmt->bindValue(':cep', $_POST['cep']);
+	$run = $stmt->execute();
 
-	$usuarios = $jsonfile['usuarios'];
-	$usuario = $usuarios[$_SESSION['id']];
+	if ($stmt->rowCount() > 0) {
+		$row = $stmt->fetch(PDO::FETCH_OBJ);
+		$idEndereco = $row->id;
+	} else {
+		$stmt = $pdo->prepare("insert into enderecos (cep, estado, cidade, bairro, rua) values (?, ?, ?, ?, ?)");
+		$stmt->bindParam(1, $_POST['cep']);
+		$stmt->bindParam(2, $_POST['estado']);
+		$stmt->bindParam(3, $_POST['cidade']);
+		$stmt->bindParam(4, $_POST['bairro']);
+		$stmt->bindParam(5, $_POST['rua']);
+		$run = $stmt->execute();
 
-	if (!isset($usuario['imoveis'])) {
-		$usuario['imoveis'] = Array();
+		$idEndereco = $pdo->lastInsertId();
 	}
 
-	array_push($usuario['imoveis'], Array('nome' => $_POST['nome'],
-								'suites' => $_POST['suites'],
-								'quartos' => $_POST['quartos'],
-								'area_privativa' => $_POST['area_privativa'],
-								'area_total' => $_POST['area_total'],
-								'vaga_garagem' => $_POST['vaga_garagem'],
-								'cep' => $_POST['cep'],
-								'estado' => $_POST['estado'],
-								'cidade' => $_POST['cidade'],
-								'bairro' => $_POST['bairro'],
-								'rua' => $_POST['rua'],
-								'numero' => $_POST['numero'],
-								'caracteristicas' => $_POST['caracteristicas'],
-								'informacoes_adicionais' => $_POST['informacoes_adicionais']));
+	$stmt = $pdo->prepare("INSERT INTO imoveis
+		(nome, suites, quartos, area_privativa, area_total, vaga_garagem, numero, caracteristicas, informacoes_adicionais, id_endereco)
+	VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-	$usuarios[$_SESSION['id']] = $usuario;
-	$response['usuarios'] = $usuarios;
+	$stmt->bindParam(1, $_POST['nome']);
+	$stmt->bindParam(2, $_POST['suites']);
+	$stmt->bindParam(3, $_POST['quartos']);
+	$stmt->bindParam(4, $_POST['area_privativa']);
+	$stmt->bindParam(5, $_POST['area_total']);
+	$stmt->bindParam(6, $_POST['vaga_garagem']);
+	$stmt->bindParam(7, $_POST['numero']);
+	$stmt->bindParam(8, $_POST['caracteristicas']);
+	$stmt->bindParam(9, $_POST['informacoes_adicionais']);
+	$stmt->bindParam(10, $idEndereco);
 
-	$fp = fopen('../dados/usuarios.json', 'w');
-	fwrite($fp, json_encode($response));
-	fclose($fp);
+	$run = $stmt->execute();
+
 } elseif (isset($_GET['acao']) && $_GET['acao'] == 'listarImoveis') {
-	$getfile = file_get_contents('../dados/usuarios.json');
-	$jsonfile = json_decode($getfile, true);
-	$usuarios = $jsonfile['usuarios'];
+
+	$stmt = $pdo->prepare("select id, nome, caracteristicas from imoveis where id_usuario = :id_usuario");
+	$stmt->bindValue(':id_usuario', $_SESSION['id']);
+	$stmt->execute();
 
 	$htmlImoveis = '';
 
-	foreach ( $usuarios as $chave => $e ) {
-		if(isset($e['imoveis'])) {
-			foreach ($e['imoveis'] as $chaveImovel => $ee) {
-				$htmlImoveis .= '<div class="row box-imovel-busca col-md-12" id="imovel'.$chaveImovel.$chave.'">
-					<div class="informacoes-busca col-md-9 col-sm-9 col-xs-12">
-						<center><div class="img-busca col-sm-3 col-xs-12">
-							<img class="img-busca" src="./img/logo.jpg" />
-						</div></center>
-						<div class="info-imovel-busca col-sm-9 col-xs-12">
-							<p>' . $ee["nome"] . '</p>
-							<p>' . $ee["caracteristicas"] . '</p>
-						</div>
-					</div>
-					<div class="informacoes-busca col-md-3 col-sm-3 col-xs-12">
+	while($row = $stmt->fetch(PDO::FETCH_OBJ)){
+		$htmlImoveis .= '<div class="row box-imovel-busca col-md-12" id="imovel'.$row->id.'">
+			<div class="informacoes-busca col-md-9 col-sm-9 col-xs-12">
+				<center><div class="img-busca col-sm-3 col-xs-12">
+					<img class="img-busca" src="./img/logo.jpg" />
+				</div></center>
+				<div class="info-imovel-busca col-sm-9 col-xs-12">
+					<p>' . $row->nome . '</p>
+					<p>' . $row->caracteristicas . '</p>
+				</div>
+			</div>
+			<div class="informacoes-busca col-md-3 col-sm-3 col-xs-12">
 
-						<button type="button" class="btn-busca" onclick="editarImovel(' . $chaveImovel . ', ' . $chave . ');">Editar</button>
-						<button type="button" class="btn-busca" onclick="excluirImovel(' . $chaveImovel . ', ' . $chave . ');" data-toggle="modal" data-target="#excluir_imovel">Excluir</button>
-					</div>
-				</div>';
-			}
-		}
+				<button type="button" class="btn-busca" onclick="editarImovel(' . $row->id . ');">Editar</button>
+				<button type="button" class="btn-busca" onclick="excluirImovel(' . $row->id . ');" data-toggle="modal" data-target="#excluir_imovel">Excluir</button>
+			</div>
+		</div>';
 	}
 
 	echo $htmlImoveis;
 } elseif (isset($_POST['acao']) && $_POST['acao'] == 'excluirImovel') {
-	$getfile = file_get_contents('../dados/usuarios.json');
-	$jsonfile = json_decode($getfile, true);
 
-	$usuarios = $jsonfile['usuarios'];
-	unset($usuarios[$_POST['id_usuario']]['imoveis'][$_POST['id_imovel']]);
-	$response['usuarios'] = $usuarios;
-
-	$fp = fopen('../dados/usuarios.json', 'w');
-	fwrite($fp, json_encode($response));
-	fclose($fp);
+	$stmt = $pdo->prepare("delete from imoveis where id = :id_imovel");
+	$stmt->bindValue(':id_imovel', $_POST['id_imovel']);
+	$run = $stmt->execute();
 
 } elseif (isset($_POST['acao']) && $_POST['acao'] == 'buscaEditarImovel') {
-	$getfile = file_get_contents('../dados/usuarios.json');
-	$jsonfile = json_decode($getfile, true);
 
-	$usuarios = $jsonfile['usuarios'];
-	echo json_encode($usuarios[$_POST['id_usuario']]['imoveis'][$_POST['id_imovel']]);
+	$stmt = $pdo->prepare("select i.*, e.cep, e.estado, e.cidade, e.bairro, e.rua
+												from imoveis as i left join enderecos as e on(i.id_endereco = e.id)
+												where i.id = :id_imovel");
+	$stmt->bindValue(':id_imovel', $_POST['id_imovel']);
+	$stmt->execute();
+	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	echo json_encode($results);
+
 } elseif (isset($_POST['acao']) && $_POST['acao'] == 'editarImovel') {
-	$getfile = file_get_contents('../dados/usuarios.json');
-	$jsonfile = json_decode($getfile, true);
+	
+	$stmt = $pdo->prepare("select * from enderecos where cep = :cep");
+	$stmt->bindValue(':cep', $_POST['cep']);
+	$run = $stmt->execute();
 
-	$usuarios = $jsonfile['usuarios'];
+	if ($stmt->rowCount() > 0) {
+		$row = $stmt->fetch(PDO::FETCH_OBJ);
+		$idEndereco = $row->id;
 
-	$usuarios[$_POST['id_usuario']]['imoveis'][$_POST['id_imovel']] = Array(
-								'nome' => $_POST['nome'],
-								'suites' => $_POST['suites'],
-								'quartos' => $_POST['quartos'],
-								'area_privativa' => $_POST['area_privativa'],
-								'area_total' => $_POST['area_total'],
-								'vaga_garagem' => $_POST['vaga_garagem'],
-								'cep' => $_POST['cep'],
-								'estado' => $_POST['estado'],
-								'cidade' => $_POST['cidade'],
-								'bairro' => $_POST['bairro'],
-								'rua' => $_POST['rua'],
-								'numero' => $_POST['numero'],
-								'caracteristicas' => $_POST['caracteristicas'],
-								'informacoes_adicionais' => $_POST['informacoes_adicionais']);
+		if ($row->estado != $_POST['estado'] || $row->cidade != $_POST['cidade'] || $row->bairro != $_POST['bairro'] || $row->rua != $_POST['rua']) {
+			$stmt = $pdo->prepare("UPDATE enderecos set estado = ?, cidade = ?, bairro = ?, rua = ? WHERE id = ?");
+			$stmt->bindParam(1, $_POST['estado']);
+			$stmt->bindParam(2, $_POST['cidade']);
+			$stmt->bindParam(3, $_POST['bairro']);
+			$stmt->bindParam(4, $_POST['rua']);
+			$stmt->bindParam(5, $idEndereco);
+			$run = $stmt->execute();
+		}
+	} else {
+		$stmt = $pdo->prepare("insert into enderecos (cep, estado, cidade, bairro, rua) values (?, ?, ?, ?, ?)");
+		$stmt->bindParam(1, $_POST['cep']);
+		$stmt->bindParam(2, $_POST['estado']);
+		$stmt->bindParam(3, $_POST['cidade']);
+		$stmt->bindParam(4, $_POST['bairro']);
+		$stmt->bindParam(5, $_POST['rua']);
+		$run = $stmt->execute();
 
-	$response['usuarios'] = $usuarios;
+		$idEndereco = $pdo->lastInsertId();
+	}
 
-	$fp = fopen('../dados/usuarios.json', 'w');
-	fwrite($fp, json_encode($response));
-	fclose($fp);
+	$stmt = $pdo->prepare("UPDATE imoveis set nome = ?, suites = ?, quartos = ?, area_privativa = ?,
+		 										area_total = ?, vaga_garagem = ?, numero = ?, caracteristicas = ?,
+												informacoes_adicionais = ?, id_endereco = ? WHERE id = ?");
+	$stmt->bindParam(1, $_POST['nome']);
+	$stmt->bindParam(2, $_POST['suites']);
+	$stmt->bindParam(3, $_POST['quartos']);
+	$stmt->bindParam(4, $_POST['area_privativa']);
+	$stmt->bindParam(5, $_POST['area_total']);
+	$stmt->bindParam(6, $_POST['vaga_garagem']);
+	$stmt->bindParam(7, $_POST['numero']);
+	$stmt->bindParam(8, $_POST['caracteristicas']);
+	$stmt->bindParam(9, $_POST['informacoes_adicionais']);
+	$stmt->bindParam(10, $idEndereco);
+	$stmt->bindParam(11, $_POST['id_imovel']);
+	$run = $stmt->execute();
 
 }
 ?>
